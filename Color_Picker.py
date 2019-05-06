96deebddb7feafd4e6fc938d69d4f86283ee30f6
OttovonLauenburg <38391297+OttovonLauenburg@users.noreply.github.com>
2019-05-06 15:14:40 -0700

    Add files via upload

---
+++ Color_Picker.py
@@ -0,0 +1,17 @@
+Common Color Picker
+R G B
+0 0 0 = Standard Black
+255 0 0 = Standard Red
+0 255 0 = Standard Green
+0 0 255 = Standard Blue
+255 255 0 = Standard Yellow
+0 255 255 = Wathte Blue
+255 255 255 = Standard White
+
+
+Some interesting principles:
+
+If R-value = G-value = B-value（For example: 67 67 67 or 100 100 100),
+the color it expressing is a kind of gray. The bigger value is, the more
+bright the color it expressing is. Note:125 125 125 = Standard Gray
+
